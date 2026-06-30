c3ef107ecc3aa19cc6e969b134f9abccff220228
Ye Xinkang <yexinkang1234@gmail.com>
2026-06-30 21:24:42 +0800

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import { Geist, Geist_Mono, Playfair_Display } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils";
+
+const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
+
+const playfairDisplay = Playfair_Display({subsets:['latin'],variable:'--font-serif'});
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontSans.variable, fontMono.variable, "font-serif", playfairDisplay.variable, geistHeading.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
