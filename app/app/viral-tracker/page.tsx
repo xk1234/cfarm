d823ca1b7ed1cb86a3e6be6d340eacdcdf5db756
xk1234 <31469009+xk1234@users.noreply.github.com>
2026-07-29 17:36:28 +0800

    Add TikTok viral tracker

---
+++ app/app/viral-tracker/page.tsx
@@ -0,0 +1,5 @@
+import { WorkspaceRoute } from "@/components/realfarm/routes/workspace-route"
+
+export default function ViralTrackerPage() {
+  return <WorkspaceRoute navigation={{ view: "viral-tracker" }} />
+}
